c29b2de72c8221d1e1bf5e05282fb9ebee0f6bc7
Abcde Keith Leonardo <abcdeleonardo@gmail.com>
2022-11-04 15:10:07 +0800

    update

--- src/components/Main/About/About.js
+++ src/components/Main/About/About.js
@@ -13,7 +13,7 @@ const About = () => {
                 <p >My name is <strong>Abcde Keith Leonardo</strong> "AK" for short. I am a full stack web developer. I have been working in the web development field for almost a year.  I am proficient in HTML, CSS, JavaScript, React, Node, and Express. I am currently looking for a full time position as a web developer. I am also looking for freelance work. Previously was in Hotel and Restaurant industry at Centro Escolar University Now a Graduate Web Developer in Uplift Code Camp.  I love playing video games ever since a kid also watch anime or action movies.Also, growing up with passion for japanese car culture. If you have any questions, please feel free to contact me.</p>
                 <p>"One Level-UP at a grind"</p>
 
-             <button><a href="https://drive.google.com/file/d/1uUNVCU2rOqT8Ss4wG4EenQPNHyWpnIxs/view?usp=share_link" target="_blank">My Resume </a></button>
+             <button><a href="https://drive.google.com/file/d/1uUNVCU2rOqT8Ss4wG4EenQPNHyWpnIxs/view?usp=sharing" target="_blank">My Resume </a></button>
             <Link to='/contact' className={style.contact}>Contact Me</Link>
 
              </div>
